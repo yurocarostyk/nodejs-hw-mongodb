e11c7314a23ac9cce46037a40a2137b751f95dea
yurocarostyk <152785136+yurocarostyk@users.noreply.github.com>
2024-09-05 08:56:58 +0200

    hw 2

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import contactsRouter from './routers/contacts.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  app.use(cors());
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world',
+    });
+  });
+  app.use(contactsRouter);
+  app.use(errorHandler);
+  app.use('*', notFoundHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+  });
+};
