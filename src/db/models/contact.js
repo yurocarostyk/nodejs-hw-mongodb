4ff60f65ca6c8cfb795f1c8b4abe1ba298ee2f20
yurocarostyk <152785136+yurocarostyk@users.noreply.github.com>
2024-09-09 18:12:02 +0200

    hw2

---
+++ src/db/models/contact.js
@@ -0,0 +1,31 @@
+import { model, Schema } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: String,
+    isFavourite: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const contactsCollection = model('contact', contactSchema);
--- src/utils/ctrlWrapper.js
+++
@@ -1,9 +0,0 @@
-export const ctrlWrapper = (controller) => {
-  return async (req, res, next) => {
-    try {
-      await controller(req, res, next);
-    } catch (err) {
-      next(err);
-    }
-  };
-};
