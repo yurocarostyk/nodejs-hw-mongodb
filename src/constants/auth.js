19d6305342197a7242988bb54256cd2727891012
yurocarostyk <yurocarostyk@ukr.net>
2024-10-14 00:19:59 +0200

    const

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,2 +1,1 @@
-// export const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-export const emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
