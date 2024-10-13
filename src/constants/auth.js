634a29a661a89ec03b021de40dd01063898cd413
yurocarostyk <yurocarostyk@ukr.net>
2024-10-13 23:29:51 +0200

    auth

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,1 +1,2 @@
-export const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+// export const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+export const emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
